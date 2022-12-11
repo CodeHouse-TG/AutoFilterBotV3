d6688a0c4518fb23600caebae630567a9e8513e1
Adwin Mosses <100942168+Private-botz@users.noreply.github.com>
2022-12-11 09:55:04 +0530

    Initial commit

---
+++ LuciferMoringstar_Robot/__init__.py
@@ -0,0 +1,43 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+Basic Commads
+
+/start :- check am Alive
+
+Bot Owner Only
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+
+"""
+
+ABOUT = """
+➥ Name :  Auto Filter V3
+    
+➥ Creator : [Adwin Mosses](
+https://t.me/adwin_mosses)
+
+➥ DEV : [Private-Botz](https://t.me/Coding_x)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Tutorial : [Click Me](https://github.com/Private-Botz/AutoFilterBotV3)
+"""
